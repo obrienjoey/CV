56a7bf287d298dacd300300fe030641d6a1c64fc
obrienjoey <mr.joeyob@gmail.com>
2021-01-25 15:40:46 +0000

    added new publcation data

--- data/publications.R
+++ data/publications.R
@@ -4,7 +4,7 @@ publications <- tribble(
   '06, 2020', 'Quantifying uncertainty in a predictive model for popularity dynamics', "\\textbf{J.D. O'Brien}, A. Aleta, Y. Moreno, and J.P. Gleeson", 'Physical Review E', 'published',
   '06, 2020', 'A generalization of the classical Kelly betting formula to the case of temporal correlation', "\\textbf{J.D. O'Brien}, K. Burke, M.E. Burke, and B.R. Barmish", 'IEEE Control Systems Letters', 'published',
   '07, 2020', 'A generalized framework for simultaneous long-short feedback trading', "\\textbf{J.D. O'Brien},  M.E. Burke, and K. Burke", 'IEEE Trans. on Automatic Control', 'published',
-  '09, 2020', 'Identification of skill in an online game: The case of Fantasy Premier League', "\\textbf{J.D. O'Brien},  J.P. Gleeson, and D.J.P O'Sullivan", '', 'under review',
-  '10, 2020', 'A complex networks approach towards ranking professional Snooker players', "\\textbf{J.D. O'Brien}, and J.P. Gleeson", '', 'under review',
+  '01, 2021', 'Identification of skill in an online game: The case of Fantasy Premier League', "\\textbf{J.D. O'Brien},  J.P. Gleeson, and D.J.P O'Sullivan", 'PLOS One (in press)', 'published',
+  '01, 2021', 'A complex networks approach towards ranking professional Snooker players', "\\textbf{J.D. O'Brien}, and J.P. Gleeson", 'Journal of Complex Networks (in press)', 'published',
   '12, 2020', 'Hierarchical route to the emergence of leader nodes in real-world networks', "\\textbf{J.D. O'Brien},  K.A. Oliveira, J.P. Gleeson, and M. Asllani", '', 'under review'
 )
